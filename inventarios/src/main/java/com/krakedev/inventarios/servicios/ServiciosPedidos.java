a7ea315b94de14fd428c28592630c87b5422ccf6
josuetipan <josuetipan101@gmail.com>
2024-05-12 11:21:27 -0500

    recibir pedido

--- inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosPedidos.java
+++ inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosPedidos.java
@@ -2,6 +2,7 @@ package com.krakedev.inventarios.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -28,6 +29,19 @@ public class ServiciosPedidos {
 		}
 	}
 	
+	@Path("recibir")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response actualizar(Pedido pedido) {
+		PedidoBDD ped = new PedidoBDD();
+		try {
+			ped.actualizar(pedido);
+			return Response.ok().build();
+		} catch (KarkeDevExcepcion e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
 	
 	
 }
